353b0975fcbc2a2f6441aa329351920662028ac0
Brian Mwangi <momanyibrian18@gmail.com>
2023-03-14 19:42:03 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,18 @@
+
+const chai = require("chai");
+const spies = require("chai-spies");
+
+chai.use(spies);
+const expect = chai.expect;
+
+function receivesAFunction(callback) {
+  callback();
+}
+
+function returnsANamedFunction() {
+  return function namedFunction() {};
+}
+
+function returnsAnAnonymousFunction() {
+  return function() {};
+}
